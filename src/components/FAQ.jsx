ede5d858ffb033245decb9b3bb0c9bd4651cfb7d
Shrey-Patel-05 <shrey.anup.patel@gmail.com>
2025-03-03 15:56:40 +0000

    chnaged FAQ component to map from a JSON object

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,14 +1,25 @@
+import React from 'react'
+import { Divider } from '@mui/material';
+import Paper from '@mui/material/Paper';
+
 const FAQ = () => {
+    const questionData= [{"question": "Blah blah blah?", "answer": "Yes"}];
+
     return (
       <section className="py-16 px-8 bg-gray-200">
         <div className="max-w-7xl mx-auto text-center">
           <h2 className="text-3xl font-bold mb-4">Frequently Asked Questions</h2>
           <div className="space-y-4">
-            <p className="text-lg font-semibold">??</p>
-            <p>A</p>
-  
-            <p className="text-lg font-semibold">?</p>
-            <p>A</p>
+            {questionData.map((item, index) => (< div key={index} className='justify-center items-center'>
+              {/* <Paper elevation={3} /> */}
+              <p className="text-lg font-semibold">{item.question}</p>
+              <p>{item.answer}</p>
+              {/* <Paper /> */}
+              {/* <Divider className='bg-red-100 max-w-7xl mx-auto '/> */}
+              {/* <Divider flexItem sx={{ borderRightWidth: 5, height:4 }}/> */}
+              <Divider sx={{ height:2}}/>
+              
+            </div>))}
           </div>
         </div>
       </section>
