6ee8703cecc15f73f9aa910066b420fcbc761b94
Affan <affank1020@gmail.com>
2025-03-10 15:06:55 +0000

    Avoiding merge conflicts

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,41 +1,85 @@
+
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+// import IconButton from '@mui/material/IconButton';
+// import InfoIcon from '@mui/icons-material/Info';
+import { useState } from 'react';
+
 const OurTeam = () => {
-    return (
-      <section id="OurTeam" className="py-16 px-8 bg-white">
-        <div className="max-w-7xl mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-4">Meet Our Team</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div>
-              <img
-                src="https://via.placeholder.com/150"
-                alt="Team Member 1"
-                className="w-32 h-32 mx-auto rounded-full mb-4"
-              />
-              <p className="font-semibold">John Doe</p>
-              <p>Lead Developer</p>
+  const members = [{ "name": "Sean bechofer", "image": "https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg", "role": "Lead dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }, { "name": "Shrey Patel", "image": "https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=612x612&w=0&k=20&c=NtM9Wbs1DBiGaiowsxJY6wNCnLf0POa65rYEwnZymrM=", "role": "dev", "linkedin": "", "github": "" }];
+  const [plusSlides, setplusSlides] = useState(0);
+  const [carasouel, setCarasouel] = useState(false);
+
+
+  return (
+    <section className="py-16 px-8 bg-white">
+      <div className="max-w-7xl mx-auto text-center">
+        <h2 className="text-3xl font-bold mb-4">Meet Our Team</h2>
+        <div className="flex flex-col items-center justify-center">
+          <button className='text-white mb-10' onClick={() => {setCarasouel(!carasouel); setplusSlides(0);} }> {carasouel ? 'View All' : 'Carousel'}</button>
+          {
+            carasouel
+            &&
+            <div className='flex flex-row items-center justify-center'>
+              <a  className="  !p-4 !text-black  hover:!text-gray-300 !font-bold !text-3xl !transition !ease-in-out !duration-600 !rounded-r-md select-none" onClick={() => setplusSlides(Math.max(plusSlides-1, 0))}>&#10094;</a>
+
+              <ImageList sx={{ width: 800,  gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))" }} className='items-center justify-center'>
+                <ImageListItem>
+                  <img
+                    src={members[plusSlides].image}
+                    className="object-cover w-full h-full"
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={members[plusSlides].name}
+                    subtitle={members[plusSlides].role}
+                  />
+                </ImageListItem>
+            {plusSlides <= members.length-2 &&
+                <ImageListItem>
+                  <img
+                    src={members[plusSlides+1].image}
+                    className="object-cover w-full h-full"
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={members[plusSlides+1].name}
+                    subtitle={members[plusSlides+1].role}
+                  />
+                </ImageListItem>
+              }
+              </ImageList>
+              <a className="  !p-4 !text-black  hover:!text-gray-300 !font-bold !text-3xl !transition !ease-in-out !duration-600 !rounded-r-md select-none" onClick={() => setplusSlides(Math.min(plusSlides+1, members.length-1))}>&#10095;</a>
+
+
             </div>
-            <div>
-              <img
-                src="https://via.placeholder.com/150"
-                alt="Team Member 2"
-                className="w-32 h-32 mx-auto rounded-full mb-4"
-              />
-              <p className="font-semibold">Jane Smith</p>
-              <p>Designer</p>
-            </div>
-            <div>
-              <img
-                src="https://via.placeholder.com/150"
-                alt="Team Member 3"
-                className="w-32 h-32 mx-auto rounded-full mb-4"
-              />
-              <p className="font-semibold">Chris Lee</p>
-              <p>Project Manager</p>
-            </div>
-          </div>
+          }
+
+          {!carasouel && <ImageList sx={{ width: 800 }}>
+
+            {members.map((member, index) => (
+
+              <ImageListItem key={index}>
+                <img
+                  src={member.image}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={member.name}
+                  subtitle={member.role}
+                />
+              </ImageListItem>
+
+
+            ))}
+          </ImageList>
+          }
         </div>
-      </section>
-    );
-  };
-  
-  export default OurTeam;
-  
+      </div>
+    </section>
+  );
+};
+
+export default OurTeam;
